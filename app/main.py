f5b4e9378a4177a4d50871f9d4a837d8247c8a65
furimu1234 <enu7693@gmail.com>
2022-12-20 00:08:26 +0900

    demo.pyのkeep_alive関数を呼び出し、実行

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import discord
-from flask import Flask
+from demo import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -7,8 +7,6 @@ client = discord.Client(intents=intents)
 
 # 後でtokenをどうにかする
 TOKEN = "MTAxMzE2NjMwNjgzMDM0MDEzNw.GGDFdB.Lb8YPeN973-0EL4j_7gYq6Vvu9LuzJdDHdWk9Y"
-
-PORT = 8000
 
 @client.event
 async def on_ready():
@@ -23,15 +21,9 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
 
+
 print("1")
-app = Flask(__name__)
+keep_alive()
 print("2")
 client.run(TOKEN)
 print ("3")
-
-@app.route('/', methods=['###'])
-def index():
-    return 'hello world'
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=False)
