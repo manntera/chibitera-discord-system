877ce6eda3343bdd7895b3ca460b1ea698d615ef
manntera <studio@manntera.com>
2022-12-19 22:27:37 +0900

    HTTPサーバーの実装を削除しました。

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 import discord
-import http.server
-from threading import Thread
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,10 +7,7 @@ client = discord.Client(intents=intents)
 # 後でtokenをどうにかする
 TOKEN = "MTAxMzE2NjMwNjgzMDM0MDEzNw.GGDFdB.Lb8YPeN973-0EL4j_7gYq6Vvu9LuzJdDHdWk9Y"
 
-
 PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
-
 
 @client.event
 async def on_ready():
@@ -25,18 +20,3 @@ async def on_message(message):
     await message.channel.send(message.content)
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-
-def run_http_server():
-    print("start http server")
-    with http.server.HTTPServer(("/", PORT), Handler) as httpd:
-        print("serving at port", PORT)
-        httpd.serve_forever()
-        
-http_thread = Thread(target=run_http_server)
-http_thread.start()
-http_thread.join()
-        
-bot_thread = Thread(target=client.run, args=(TOKEN, ))
-bot_thread.start()
-bot_thread.join()
-
