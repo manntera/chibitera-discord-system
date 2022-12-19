a257b644350e97d88f04f0e36ea0c9dfd0d5904c
furimu1234 <enu7693@gmail.com>
2022-12-19 21:21:12 +0900

    Revert "botファイル。demo.pyをインポート"
    This reverts commit 236e2f2004affe720f7225eb89cdff129cab7e46.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import discord
-import demo
+import http.server
 from threading import Thread
 
 intents = discord.Intents.default()
@@ -8,6 +8,11 @@ client = discord.Client(intents=intents)
 
 # 後でtokenをどうにかする
 TOKEN = "MTAxMzE2NjMwNjgzMDM0MDEzNw.GGDFdB.Lb8YPeN973-0EL4j_7gYq6Vvu9LuzJdDHdWk9Y"
+
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
 
 @client.event
 async def on_ready():
@@ -21,8 +26,15 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-
-demo.keep_alive()
+def run_http_server():
+    print("start http server")
+    with http.server.HTTPServer(("/", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+        
+http_thread = Thread(target=run_http_server)
+http_thread.start()
+http_thread.join()
         
 bot_thread = Thread(target=client.run, args=(TOKEN, ))
 bot_thread.start()
