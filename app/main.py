3e53b777baeca3e91083b7f3929dd90b33bcbde0
furimu1234 <enu7693@gmail.com>
2022-12-21 05:13:49 +0900

    システム環境変数にトークンが見つからないときに.envからトークンを読み取るように変更

--- app/main.py
+++ app/main.py
@@ -9,6 +9,38 @@ client = discord.Client(intents=intents)
 # 後でtokenをどうにかする
 TOKEN = os.getenv('DISCORD_TOKEN_SECRET')
 
+def exit(msg: str) -> None:
+    """
+    処理を中断する時に実行される関数
+
+    Parameters
+    ----------
+    msg: str
+        終了する時に出力するメッセージ
+
+    Returns
+    -------
+    None
+
+    """
+    print(msg)
+    import sys
+    sys.exit()
+
+
+if TOKEN is None:
+    try:
+        from dotenv import load_dotenv
+    except ImportError:
+        exit("TOKENが見つからなかったため、処理を中断しました。")
+
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN_SECRET')
+
+    if TOKEN is None:
+        exit("TOKENが見つからなかったため、処理を中断しました。")
+
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
